0e5fe0c40321ffaf5efccc006dc57496f5075078
mrakhgari <mr.akhgari403@aut.ac.ir>
2020-02-24 00:29:40 +0330

    login form completed

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,5 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
-# from froms import LoginForm
 from app.forms import LoginForm
 
 # when called http://127.0.0.1:5000/ or http://127.0.0.1:5000/index called this method
@@ -22,8 +21,15 @@ def index():
     # to render the index.html in template folder use this method.
     return render_template( 'index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    # validate_on_submit return false if call GET.
+    if form.validate_on_submit():
+        # show a message. must be handled in html files (look at base.html) 
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        # best way use url
+        return redirect(url_for('index'))   
     return render_template('login.html', title='Sign In', form=form)
 
